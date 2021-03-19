3c9c5bcfc26b6d1e8cc6b63988f040fba974f267
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2021-03-19 13:44:32 +0100

    Correcting delete redirectory

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors())
 app.use(fileUpload({
     createParentPath: true
 }));
+//To correct error 404 at refresh
 app.use(history({
     rewrites: [
         { from: /\/users/, to: '/users'}
